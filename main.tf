bd03202c519d4efc09b82fd4af4752334371cd10
djlittle <dmrk_little@yahoo.com>
2026-01-16 10:05:36 -0500

    updated the zip path

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "aws_lambda_function" "resume_api" {
   handler       = "app.app_lambda_handler"
   runtime       = "python3.12"
 
-  filename         = "${path.module}/lambda/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda/lambda.zip")
+  filename         = "${path.module}/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
 
   environment {
     variables = {
